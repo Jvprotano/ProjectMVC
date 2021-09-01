277113d71e04bd79ca7403492f8156620b53cc8c
Jose Vinicius <jvprotano@gmail.com>
2021-09-01 11:32:24 -0300

    First Commit

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Salles Web MVC App from C# Course";
-            ViewData["Professor"] = "Nelio Alves";
+            ViewData["Message"] = "Salles Web MVC";
+            ViewData["Familia"] = "Protano";
 
             return View();
         }
